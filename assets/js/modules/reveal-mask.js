6c124c28fe6713286d95c207b3cee19d75f51743
Lay Kimhoung <houngjk081@gmail.com>
2026-07-19 19:11:13 +0700

    Done Home page

--- assets/js/modules/reveal-mask.js
+++ assets/js/modules/reveal-mask.js
@@ -1,11 +1,38 @@
-const wrapper = document.querySelector(".image-wrapper");
+function initRevealMask() {
+    const wrapper = document.querySelector(".image-wrapper");
 
-if (wrapper) {
+    if (!wrapper) return;
+
+    let mouseX = 0;
+    let mouseY = 0;
+
+    wrapper.style.setProperty("--mouse-x", "50%");
+    wrapper.style.setProperty("--mouse-y", "50%");
+    wrapper.style.setProperty("--reveal-size", "0px");
+
+    function updateReveal() {
+        wrapper.style.setProperty("--mouse-x", `${mouseX}px`);
+        wrapper.style.setProperty("--mouse-y", `${mouseY}px`);
+
+        requestAnimationFrame(updateReveal);
+    }
+
+    updateReveal();
+
     wrapper.addEventListener("mouseenter", () => {
-        wrapper.classList.add("revealed");
+        wrapper.style.setProperty("--reveal-size", "230px");
+    });
+
+    wrapper.addEventListener("mousemove", (e) => {
+        const rect = wrapper.getBoundingClientRect();
+
+        mouseX = e.clientX - rect.left;
+        mouseY = e.clientY - rect.top;
     });
 
     wrapper.addEventListener("mouseleave", () => {
-        wrapper.classList.remove("revealed");
+        wrapper.style.setProperty("--reveal-size", "0px");
     });
 }
+
+initRevealMask();
